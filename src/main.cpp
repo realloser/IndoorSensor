5c23bd707234c10adc2693ac8d5cdea04f5f731e
Martyn Messerli <realloser@gmx.ch>
2018-07-13 13:29:07 +0200

    Blink sample

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+#define VOLTAGE_INPUT 10
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  Serial.println("Blink");
+  delay(1000);
 }
