0c9247e300d8c2b1a19a55dec057f6c97cfeebba
Martyn Messerli <realloser@gmx.ch>
2018-07-13 14:29:37 +0200

    Rename variables

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,8 @@
 
 DHT dht(DHT_INPUT, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 //Variables
-int chk;
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
+float dhtHum;  //Stores humidity value
+float dhtTemp; //Stores temperature value
 
 // Reference voltage variables
 float voltage_read;
@@ -51,10 +50,10 @@ void readVoltage() {
 
 void readDHT() {
   //Read data and store it to variables hum and temp
-  hum = dht.readHumidity();
-  temp= dht.readTemperature();
-  Serial.print("Humitidty:   "); Serial.println(hum);
-  Serial.print("Temperature: "); Serial.println(temp);
+  dhtHum = dht.readHumidity();
+  dhtTemp= dht.readTemperature();
+  Serial.print("Humitidty:   "); Serial.println(dhtHum);
+  Serial.print("Temperature: "); Serial.println(dhtTemp);
 }
 
 void readLightSensor() {
