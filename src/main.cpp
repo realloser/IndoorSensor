5d6d5c0d7b484a7592f3caeda3fd063d7e3b4f1a
Martyn Messerli <realloser@gmx.ch>
2018-07-13 14:10:16 +0200

    Read humidity and temperature

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,57 @@
 
 #include <Arduino.h>
 
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// Voltage devider
 #define VOLTAGE_INPUT 10
+#define VOLTAGE_DEVIDER_FACTOR 2.355f
+
+// DHT22 Sensor
+#define DHTPIN 8     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+float voltage_read;
+float voltage_reference;
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(VOLTAGE_INPUT, INPUT);
+  dht.begin();
+
   Serial.begin(9600);
+}
+
+void readVoltage() {
+  voltage_read = analogRead(VOLTAGE_INPUT);
+  Serial.print("Voltage int:"); Serial.println(voltage_read);
+
+  voltage_reference = (VOLTAGE_DEVIDER_FACTOR*1024)/voltage_read;
+  Serial.print("Voltage Reference :"); Serial.println(voltage_reference);
+}
+
+void readDHT() {
+  //Read data and store it to variables hum and temp
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  Serial.print("Humitidty:   "); Serial.println(hum);
+  Serial.print("Temperature: "); Serial.println(temp);
 }
 
 void loop()
 {
+
+  readVoltage();
+  readDHT();
+
+
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
